0664239a878d63a45a3da2240ac9410f43c15f1d
Enrico Restuccia <enrico.rest98@gmail.com>
2022-03-26 18:38:20 +0100

    2_Object: Object manipulation

---
+++ 2_Object/Object.java
@@ -0,0 +1,29 @@
+class Object{
+    public static void main(String[] args){
+        System.out.println("Reference:");
+
+        Helper helper = new Helper();
+        helper.print();
+
+        Helper.changeXFail(helper.x, 15);
+        helper.print("Non è ancora cambiato: ");
+
+        Helper.changeXSuccess(helper, 15);
+        helper.print("E ora si: ");
+
+        helper.y = 30;
+
+        // possibile utilizzo dello scope:
+        // attualmente x = 15 e y = 30
+        {
+            // Dichiariamo tmp dentro un nested scope così da non conservarlo in memoria per tutta la durata del main
+            int tmp = helper.x;
+            helper.x = helper.y;
+            helper.y = tmp;
+        }
+        helper.print();
+        System.out.println("Y in helper vale: " + helper.y);
+        // ERRORE:
+        // System.out.println("tmp vale: " + tmp);
+    }
+}
