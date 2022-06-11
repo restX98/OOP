b6d20bc016b2d8a20ae016987660271158992fb0
Enrico Restuccia <enrico.rest98@gmail.com>
2022-06-12 00:44:43 +0200

    3_Inheritance

---
+++ PRJ_0-GetStarted/3_Inheritance/Inheritance.java
@@ -0,0 +1,23 @@
+public class Inheritance {
+
+    public static void main(String[] args) {
+
+        System.out.println("INHERITANCE");
+
+        LabeledPoint p1 = new LabeledPoint("Ciccio");
+        Point p2 = new Point(10, 10);
+        Point p3 = new Point();
+
+        System.out.println(p1.toString());
+        System.out.println(p1.equals(p3));
+
+        Point genericPoint = new LabeledPoint("Paccio", 1, 3);
+        // LabeledPoint genericPoint = new Point("Paccio", 1, 3); // Error: LabeledPoint è un Point 
+                                                                  //        ma Point non è un LabeledPoint
+
+        ((LabeledPoint)genericPoint).setLabel("Paccio1"); //Però perdo l'accesso ai nuovi metodi definiti
+        
+        System.out.println(genericPoint.toString()); // Ma uso quelli Overrided
+    }
+
+}
