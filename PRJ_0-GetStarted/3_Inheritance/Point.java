2d13c31caa1e5ac021af71ea8510139ba414ca8e
Enrico Restuccia <enrico.rest98@gmail.com>
2022-06-12 00:49:12 +0200

    3_Inheritance

---
+++ PRJ_0-GetStarted/3_Inheritance/Point.java
@@ -0,0 +1,33 @@
+public class Point {
+
+    protected int x;
+    protected int y;
+
+    public Point(){
+        this(0, 0);
+    }
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString(){
+        return this.getClass().getName() + "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+
+        /**
+        * Check if o is an instance of Point or not
+        * "null instanceof [type]" also returns false 
+        */ 
+        if (!(o instanceof Point)) return false;
+
+        Point p = (Point)o;
+        return this.x == p.x && this.y == p.y;
+    }
+}
